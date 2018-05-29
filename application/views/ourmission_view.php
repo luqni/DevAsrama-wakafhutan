a7a93c01e636f150d7f95d156be6e9aa572bf091
Muhammad Luqni Baehaqi <mluqnibaehaqi@gmail.com>
2018-05-29 09:56:03 +0700

    upload project

---
+++ application/views/ourmission_view.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Hope Center | Mission</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-1.6.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/cufon-yui.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/cufon-replace.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/Vegur_700.font.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/Vegur_400.font.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/Vegur_300.font.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/atooltip.jquery.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/script.js"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<style type="text/css">.box1 figure{behavior:url("js/PIE.htc");}</style>
+<![endif]-->
+</head>
+<body id="page2">
+<div class="body1">
+  <div class="main">
+    <!-- header -->
+    <header>
+      <div class="wrapper">
+        <h1><a href="<?php echo base_url(); ?>" id="logo">Wakaf Hutan</a></h1>
+        <nav>
+          <ul id="top_nav">
+            <li><a href="index.html"><img src="images/top_icon1.gif" alt=""></a></li>
+            <li><a href="#"><img src="images/top_icon2.gif" alt=""></a></li>
+            <li class="end"><a href="contact.html"><img src="images/top_icon3.gif" alt=""></a></li>
+          </ul>
+        </nav>
+        <nav>
+          <ul id="menu">
+            <li id="menu_active"><a href="<?php echo base_url(); ?>">Home</a></li>
+            <li><a href="<?php echo site_url('Ourmission'); ?>">Our Mission</a></li>
+            <li><a href="<?php echo site_url('News'); ?>">News &amp; Press</a></li>
+            <li><a href="help.html">How to Help</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+    <!-- / header -->
+    <!-- content -->
+    <article id="content">
+      <div class="wrapper">
+        <div class="box1">
+          <div class="line1">
+            <div class="line2 wrapper">
+              <section class="col1">
+                <h2><strong>H</strong>ope</h2>
+                <div class="pad_bot1">
+                  <figure><img src="images/page2_img1.jpg" alt=""></figure>
+                </div>
+                Hope Center is one of free website tem- plates by TemplateMonster.com, opti- mized for 1024X768 res. <a href="#" class="button1">Read More</a> </section>
+              <section class="col1 pad_left1">
+                <h2 class="color2"><strong>B</strong>elief</h2>
+                <div class="pad_bot1">
+                  <figure><img src="images/page2_img2.jpg" alt=""></figure>
+                </div>
+                This Hope Center Template goes with two packages – with PSD source files and without them.<a href="#" class="button1 color2">Read More</a> </section>
+              <section class="col1 pad_left1">
+                <h2 class="color3"><strong>H</strong>elp</h2>
+                <div class="pad_bot1">
+                  <figure><img src="images/page2_img3.jpg" alt=""></figure>
+                </div>
+                This website template has several pages:<a href="index.html" class="link1">Home</a>,<a href="mission.html" class="link1">Our Mission</a>,<a href="news.html" class="link1">News and Press</a>,<a href="help.html" class="link1">How to Help</a>,<a href="contact.html" class="link1">Contact Us</a>.<a href="#" class="button1 color3">Read More</a> </section>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="wrapper">
+        <div class="wrapper">
+          <h3>Our Mission</h3>
+          <p class="quot">  Donasi Hutan Wakaf ini adalah wujud dari tindakan konservasi secara langsung melalui pembelian lahan kritis. Nantinya akan diperuntukkan untuk membangun hutan yang berfungsi secara ekologis, baik sebagai sumber mata air, maupun sebagai penyerap karbon, tempat bersarangnya burung-burung, lebah madu, primata dan species lainnya, seterusnya akan diwakafkan dan disertifikatkan atas nama semua orang yang telah menyumbang.
+            <img src="<?php echo base_url(); ?>/assets/images/quot2.png" alt=""> </p>
+        </div>
+        <p class="pad_left2">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident:</p>
+      </div>
+      <div class="wrapper">
+        <div class="box2">
+          <div class="wrapper">
+            <section class="col1">
+              <ul class="list1">
+                <li><a href="#">Sed ut perspiciatis unde omnis</a></li>
+                <li class="color2"><a href="#">Iste natus error sit voluptatem accus</a></li>
+                <li class="color3"><a href="#">Antium doloremque totam remiam</a></li>
+              </ul>
+            </section>
+            <section class="col1 pad_left1">
+              <ul class="list1">
+                <li><a href="#">Eaque ipsa quae ab illo inventore</a></li>
+                <li class="color2"><a href="#">Veritatis et quasi architecto beatae</a></li>
+                <li class="color3"><a href="#">Dicta sunt explicabo enim ipsam</a></li>
+              </ul>
+            </section>
+            <section class="col1 pad_left1">
+              <ul class="list1">
+                <li><a href="#">Voluptatem quia voluptas sit</a></li>
+                <li class="color2"><a href="#">Aspernatur aut odit aut fugit sed quia</a></li>
+                <li class="color3"><a href="#">Consequuntur magni dolores eos</a></li>
+              </ul>
+            </section>
+          </div>
+        </div>
+      </div>
+    </article>
+    <!-- / content -->
+    <!-- footer -->
+    <footer>
+      <div class="wrapper"> <a href="index.html" id="footer_logo"><span>Hope</span>Center</a>
+        <ul id="icons">
+          <li><a href="#" class="normaltip"><img src="images/icon1.gif" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon2.gif" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon3.gif" alt=""></a></li>
+        </ul>
+      </div>
+      <div class="wrapper">
+        <nav>
+          <ul id="footer_menu">
+            <li><a href="index.html">Home</a></li>
+            <li class="active"><a href="mission.html">Our Mission</a></li>
+            <li><a href="news.html">News &amp; Press</a></li>
+            <li><a href="help.html">How to Help</a></li>
+            <li class="end"><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+        <div class="tel"><span>+1 800</span>123 45 67</div>
+      </div>
+      <div id="footer_text">Copyright &copy; <a href="#">Domain Name</a> All Rights Reserved<br>
+        Design by <a target="_blank" href="http://www.templatemonster.com/">TemplateMonster.com</a></div>
+    </footer>
+    <!-- / footer -->
+  </div>
+</div>
+<script type="text/javascript">Cufon.now();</script>
+</body>
+</html>
---
+++ application/views/welcome_message.php
@@ -0,0 +1,302 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Wakaf Hutan</title>
+<meta charset="utf-8">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/reset.css" type="text/css" media="all">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/layout.css" type="text/css" media="all">
+<link rel="stylesheet" href="<?php echo base_url(); ?>assets/css/style.css" type="text/css" media="all">
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery-1.6.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/cufon-yui.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/cufon-replace.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/Vegur_700.font.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/Vegur_400.font.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/Vegur_300.font.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.easing.1.3.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/tms-0.3.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/tms_presets.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/backgroundPosition.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/atooltip.jquery.js"></script>
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/script.js"></script>
+<!--[if lt IE 9]>
+<script type="text/javascript" src="js/html5.js"></script>
+<style type="text/css">.box1 figure{behavior:url("js/PIE.htc");}</style>
+<![endif]-->
+</head>
+<body id="page1">
+<div class="body1">
+  <div class="main">
+    <!-- header -->
+    <header>
+      <div class="wrapper">
+        <h1><a href="<?php echo base_url(); ?>" id="logo">Wakaf Hutan</a></h1>
+        <nav>
+          <ul id="top_nav">
+            <li><a href="index.html"><img src="<?php echo base_url(); ?>assets/images/top_icon1.gif" alt=""></a></li>
+            <li><a href="#"><img src="<?php echo base_url(); ?>assets/images/top_icon2.gif" alt=""></a></li>
+            <li class="end"><a href="contact.html"><img src="<?php echo base_url(); ?>assets/images/top_icon3.gif" alt=""></a></li>
+          </ul>
+        </nav>
+        <nav>
+          <ul id="menu">
+            <li id="menu_active"><a href="<?php echo base_url(); ?>">Home</a></li>
+            <li><a href="<?php echo site_url('Ourmission'); ?>">Our Mission</a></li>
+            <li><a href="<?php echo site_url('News'); ?>">Campaign</a></li>
+            <li><a href="help.html">How to Help</a></li>
+            <li><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+      </div>
+      <div class="slider">
+        <ul class="items">
+          <li> <img src="<?php echo base_url(); ?>assets/images/img1.png" alt="">
+            <div class="banner">
+              <div class="wrapper"><span>“Our<em>Mission</em>is to<em>Help</em></span></div>
+              <div class="wrapper"><strong>Those Who<em>Need</em>It”</strong></div>
+            </div>
+          </li>
+          <li> <img src="<?php echo base_url(); ?>assets/images/img2.png" alt="">
+            <div class="banner">
+              <div class="wrapper"><span>“TOGETHER we can CHANGE“</span></div>
+              <div class="wrapper"><span></span></div>
+            </div>
+          </li>
+          <li> <img src="<?php echo base_url(); ?>assets/images/img3.png" alt="">
+            <div class="banner">
+              <div class="wrapper"><span>“Let's Donation“</span></div>
+              <div class="wrapper"><span></span></div>
+            </div>
+          </li>
+        </ul>
+        <ul class="pagination">
+          <li id="banner1"><a href="#">Make<span>Campaign</span></a></li>
+          <li id="banner2"><a href="#">join<span>volunteer</span></a></li>
+          <li id="banner3"><a href="#">Let's<span>Donate</span></a></li>
+        </ul>
+      </div>
+    </header>
+    <!-- / header -->
+    <!-- content -->
+    <article id="content">
+      <div class="wrapper">
+        <div class="box1">
+          <div class="line1">
+            <div class="line2 wrapper">
+              <section class="col1">
+                <h2><strong>C</strong>ampaign<span> Satu</span></h2>
+                <div class="pad_bot1">
+                  <figure><img src="images/page1_img1.png" alt=""></figure>
+                </div>
+                Hope Center is one of free website tem- plates by TemplateMonster.com, opti- mized for 1024X768 res. <a href="#" class="button1">Read More</a> </section>
+              <section class="col1 pad_left1">
+                <h2 class="color2"><strong>C</strong>ampaign<span> Dua</span></h2>
+                <div class="pad_bot1">
+                  <figure><img src="images/page1_img2.png" alt=""></figure>
+                </div>
+                This Hope Center Template goes with two packages – with PSD source files and without them.<a href="#" class="button1 color2">Read More</a> </section>
+              <section class="col1 pad_left1">
+                <h2 class="color3"><strong>C</strong>ampaign<span> Tiga</span></h2>
+                <div class="pad_bot1">
+                  <figure><img src="images/page1_img3.png" alt=""></figure>
+                </div>
+                This website template has several pages:<a href="index.html" class="link1">Home</a>,<a href="<?php echo site_url('Ourmission'); ?>" class="link1">Our Mission</a>,<a href="news.html" class="link1">News and Press</a>,<a href="help.html" class="link1">How to Help</a>,<a href="contact.html" class="link1">Contact Us</a>.<a href="#" class="button1 color3">Read More</a> </section>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="wrapper">
+        <h3>Our Mission</h3>
+        <p class="quot"> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incid- idunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exerci- tation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse.<img src="images/quot2.png" alt=""> </p>
+      </div>
+      <div class="wrapper">
+        <div class="box2">
+          <div class="line1">
+            <div class="line2 wrapper">
+              <section class="col1">
+                <h4><span>June 31</span>2011</h4>
+                <p class="pad_bot2"><strong>Ned ut perspiciatis unde omnis</strong></p>
+                <p>Natus error sit voluptatem accusantium doloremque laudantium.</p>
+                <a href="#" class="button2">Read More</a> </section>
+              <section class="col1 pad_left1">
+                <h4 class="color2"><span>June 29</span>2011</h4>
+                <p class="pad_bot2"><strong>At veroeos et accusamus etiusto</strong></p>
+                <p>Architecto beatae vitae dicta sunt explicabo emo enim ipsam.</p>
+                <a href="#" class="button2 color2">Read More</a> </section>
+              <section class="col1 pad_left1">
+                <h4 class="color3"><span>June 17</span>2011</h4>
+                <p class="pad_bot2"><strong>Temporibus quibusdam</strong></p>
+                <p>Magni dolores eos qui ratione voluptatem sequi nesciunt.</p>
+                <a href="#" class="button2 color3">Read More</a> </section>
+            </div>
+          </div>
+        </div>
+      </div>
+    </article>
+    <!-- / content -->
+    <!-- footer -->
+    <footer>
+      <div class="wrapper"> <a href="index.html" id="footer_logo"><span>Hope</span>Center</a>
+        <ul id="icons">
+          <li><a href="#" class="normaltip"><img src="images/icon1.gif" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon2.gif" alt=""></a></li>
+          <li><a href="#" class="normaltip"><img src="images/icon3.gif" alt=""></a></li>
+        </ul>
+      </div>
+      <div class="wrapper">
+        <nav>
+          <ul id="footer_menu">
+            <li class="active"><a href="index.html">Home</a></li>
+            <li><a href="mission.html">Our Mission</a></li>
+            <li><a href="news.html">News &amp; Press</a></li>
+            <li><a href="help.html">How to Help</a></li>
+            <li class="end"><a href="contact.html">Contact</a></li>
+          </ul>
+        </nav>
+        <div class="tel"><span>+1 800</span>123 45 67</div>
+      </div>
+      <div id="footer_text">Copyright &copy; <a href="#">Domain Name</a> All Rights Reserved<br>
+        Design by <a target="_blank" href="http://www.templatemonster.com/">TemplateMonster.com</a></div>
+    </footer>
+    <!-- / footer -->
+  </div>
+</div>
+<script type="text/javascript">Cufon.now();</script>
+<script type="text/javascript">
+$(window).load(function () {
+    $('.slider')._TMS({
+        preset: 'zabor',
+        easing: 'easeOutQuad',
+        duration: 800,
+        pagination: true,
+        banners: true,
+        waitBannerAnimation: false,
+        slideshow: 6000,
+        bannerShow: function (banner) {
+            banner.css({
+                right: '-700px'
+            }).stop().animate({
+                right: '0'
+            }, 600, 'easeOutExpo')
+        },
+        bannerHide: function (banner) {
+            banner.stop().animate({
+                right: '-700'
+            }, 600, 'easeOutExpo')
+        }
+    })
+    $('.pagination li').hover(function () {
+        if (!$(this).hasClass('current')) {
+            $(this).find('a').stop().animate({
+                backgroundPosition: '0 0'
+            }, 600, 'easeOutExpo', function () {
+                $(this).parent().css({
+                    backgroundPosition: '-20px 0'
+                })
+            });
+        }
+    }, function () {
+        if (!$(this).hasClass('current')) {
+            $(this).css({
+                backgroundPosition: '0 0'
+            }).find('a').stop().animate({
+                backgroundPosition: '-250px 0'
+            }, 600, 'easeOutExpo');
+        }
+    })
+})
+</script>
+</body>
+</html>
+
+
+
+
+<!-- <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+
+	<style type="text/css">
+
+	::selection { background-color: #E13300; color: white; }
+	::-moz-selection { background-color: #E13300; color: white; }
+
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+
+	#body {
+		margin: 0 15px 0 15px;
+	}
+
+	p.footer {
+		text-align: right;
+		font-size: 11px;
+		border-top: 1px solid #D0D0D0;
+		line-height: 32px;
+		padding: 0 10px 0 10px;
+		margin: 20px 0 0 0;
+	}
+
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		box-shadow: 0 0 8px #D0D0D0;
+	}
+	</style>
+</head>
+<body>
+
+<div id="container">
+	<h1>Welcome to CodeIgniter!</h1>
+
+	<div id="body">
+		<p>The page you are looking at is being generated dynamically by CodeIgniter.</p>
+
+		<p>If you would like to edit this page you'll find it located at:</p>
+		<code>application/views/welcome_message.php</code>
+
+		<p>The corresponding controller for this page is found at:</p>
+		<code>application/controllers/Welcome.php</code>
+
+		<p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+	</div>
+
+	<p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+</div>
+
+</body>
+</html> -->
